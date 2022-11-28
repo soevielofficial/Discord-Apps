cd5649b1009b2b7222697282908ab4ccccbe7ee1
soevielofficial <soevielofficial@gmail.com>
2022-11-28 21:20:29 +0700

    new file:   .gitignore 	new file:   index.js 	new file:   package.json 	new file:   welcomeimage.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const generateImage = require("./welcomeimage")
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+})
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.content == "hi"){
+        message.reply("Hello World!")
+    }
+})
+
+const welcomeChannelId = "1046774871281123389"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+
+client.login(process.env.TOKEN)
