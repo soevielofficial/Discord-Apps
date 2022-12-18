dc5a863d2bee81a45d7810d994a7c219e9ec2890
soevielofficial <soevielofficial@gmail.com>
2022-12-18 23:16:58 +0700

    modified:   package.json 	modified:   welcomeimage.js

--- welcomeimage.js
+++ welcomeimage.js
@@ -17,7 +17,7 @@ const av = {
 const generateImage = async (member) => {
     let username = member.user.username
     let discrim = member.user.discriminator
-    let avatarURL = member.user.displayAvatarURL({format: "png", dynamic: false, size: av.size})
+    let avatarURL = member.user.displayAvatarURL({extension: 'png', dynamic: false, size: av.size})
 
     const canvas = Canvas.createCanvas(dim.width, dim.height)
     const ctx = canvas.getContext("2d")
@@ -57,7 +57,7 @@ const generateImage = async (member) => {
     ctx.font = "40px Sans"
     ctx.fillText("to the server", dim.width / 2, dim.height - dim.margin - 50)
 
-    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.png")
+    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), "welcome.png")
     return attachment
 }
 
